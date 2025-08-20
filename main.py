4e4c53a9ddb7a5d99923940670959028e040fb0a
dylansawicki15 <dylansawicki15@gmail.com>
2025-08-20 20:12:19 +0200

    verbose flag to hide details + calc app for llm to fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
 from dotenv import load_dotenv
-from google import genai
+from google import genai 
+from google.genai import types
 
 load_dotenv()
 
@@ -9,15 +10,30 @@ api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    input_text = sys.argv[1]
+    if len(sys.argv) == 1:
+        print("Must provide one argument")
+        sys.exit(-1)
+    
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        verbose = True
+    else:
+        verbose = False
+
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=input_text,
+        contents=messages,
     )
 
     print(response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     
 
 if __name__ == "__main__":
